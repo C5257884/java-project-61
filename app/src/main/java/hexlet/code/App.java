ea8f1b24cdc915705d4cd753179ec9812f6b2348
Alexander Alexejev <maxlib2@yandex.ru>
2024-12-07 18:36:40 +0300

    step 3: new Cli with Scaner

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,10 @@
 package hexlet.code;
 
 public class App {
+    public static final String GREET_MSG = "Welcome to the Brain Games!";
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(GREET_MSG);
+        var name = Cli.getUserName("May I have your name?");
+        System.out.println("Hello, " + name + "!");
     }
 }
