ad0f57b352196766232675a0435b6ae2bc857665
Alexander Alexejev <maxlib2@yandex.ru>
2024-12-22 15:03:29 +0300

    Step 10: check before "BIG CHEСKING" 06 1. fix: greeting message - adding <\n> symbol

--- app/src/main/java/hexlet/code/game/Greet.java
+++ app/src/main/java/hexlet/code/game/Greet.java
@@ -3,7 +3,7 @@ package hexlet.code.game;
 import hexlet.code.Cli;
 
 public class Greet extends AbstractGame {
-    public static final String GREET_MSG = "Welcome to the Brain Games!";
+    public static final String GREET_MSG = "\nWelcome to the Brain Games!";
     public static final String INPUT_YOUR_NAME = "May I have your name?";
     public static final String HELLO_MSG = "Hello, ";
 
