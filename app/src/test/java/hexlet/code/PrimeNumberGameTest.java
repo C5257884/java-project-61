b0c6929a267c31d9a55166e57a16e70c9b4ce653
Alexander Alexejev <maxlib2@yandex.ru>
2024-12-22 14:14:19 +0300

    Step 10: check before "BIG CHEСKING" 03 1. fix: MagicNumber in UnitTest

--- app/src/test/java/hexlet/code/PrimeNumberGameTest.java
+++ app/src/test/java/hexlet/code/PrimeNumberGameTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PrimeNumberGameTest {
-
+    @SuppressWarnings("checkCode:MagicNumber")
     @Test
     void testIsPrime() {
 
